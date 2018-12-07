22ad3b13160a3d5a801caeb242706d10323cc839
cypark1 <45478729+cypark1@users.noreply.github.com>
2018-12-07 17:46:21 +0900

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String args[]) {
-    System.out.println("Hello World!_test212222tttt2222");
+    System.out.println("Hello World!_test212222tttt2www222");
   }
 }
